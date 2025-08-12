01e9cb2a5f31703b18a8a4a2660e2ee633d7dd6c
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-08-12 20:37:22 +0300

    release v1.0

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+
+using operator_t = char;
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+
+    operator_t op;
+    double x, y, result;
+
+    cout << "*** Calculator Program ***";
+
+    cout << "What is the operator: ";
+    cin >> op;
+
+    cout << "What is the first number: ";
+    cin >> x;
+
+    cout << "What is the second number: ";
+    cin >> y;
+
+    switch (op)
+    {
+    case '+':
+        result = x + y;
+        cout << x << " + " << y << " = " << result;
+        break;
+
+    case '-':
+        result = abs(x - y);
+        cout << "|" << x << " + " << y << "|" << " = " << result;
+        break;
+
+    case '*':
+        result = x * y;
+        cout << x << " * " << y << " = " << result;
+        break;
+
+    case '/':
+        result = x / y;
+        cout << x << " / " << y << " = " << result;
+        break;
+    default:
+        cout << "Please enter one of the following operators: + or - or * or /" << "\n";
+        main();
+    }
+
+    return 0;
+}
